539d16d88c0296758c9ef66ccfc55356f835ffa4
Shivakant6387 <shivakantsingh900@gmail.com>
2024-01-18 16:36:12 +0530

    GlobalException Handler

---
+++ src/test/java/com/example/globalExceptionHandlersks/GlobalExceptionHandlersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalExceptionHandlersks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalExceptionHandlersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
