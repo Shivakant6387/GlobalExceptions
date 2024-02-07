fec657593f927d2ac5298d8e5b2eea075f2bd2a6
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-07 18:13:58 +0530

    ProductNotFoundException

---
+++ src/main/java/com/example/globalExceptionHandlersks/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.globalExceptionHandlersks.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private String id;
+    private String name;
+}
